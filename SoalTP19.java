371ee2a958920b5636bb0c178ab94c5bb746a167
arizky012 <arizki17h@student.unhas.ac.id>
2019-10-10 09:40:24 +0800

    upload soal

---
+++ SoalTP19.java
@@ -0,0 +1,41 @@
+class SoalTP19{
+    public static void main(String[] args) {
+        rules();no1();no2();no3();no4();no5();no6();
+          
+    }
+    static void no1(){
+        System.out.println("1. jelaskan apa yang dimkasud dengan method, fungsi, dan procedure ?(15)\n");
+    }
+    static void no2(){
+        System.out.println("2. apakah dalam sebuah class harus terdapat fungsi dan procedure ?(ya/tidak)(15)\n");
+    }
+    static void no3(){
+        System.out.println("3. sebutkan dan jelaskan bagian-bagian dari method !(20)\n");
+    }
+    static void no4(){
+        System.out.println("4. tuliskan output dari program dibawah! (25)\n");
+        System.out.println(" import java.util.Scanner;\n class Contoh{");
+        System.out.println("  public static void main(String [] hehehe){");
+        System.out.println("    Scanner wkwk = new Scanner(System.in);");
+        System.out.println("    int n = wkwk.nextInt(); // digit kedua terakhir nim anda");
+        System.out.println("    int m = wkwk.nextInt(); // digit terakhir nim anda");
+        System.out.println("    jumlah(n+1,m+1);");
+        System.out.println("  }");
+        System.out.println("  static void jumlah(int a,int b){");
+        System.out.println("    System.out.println(a*a+a*b+b);");
+        System.out.println("  }\n }\n");
+    }
+    static void no5(){ 
+        System.out.println("5. apa yang terjadi apa terjadi bila method non static di panggil pada main method ? (10)\n");
+        
+    }
+    static void no6(){
+        System.out.println("6. kumpul TP(15)\n");
+    }
+    static void rules(){
+        System.out.println("**tidak dilubangi : -5");
+        System.out.println("**coretan : -1");
+        System.out.println("**tidak dihekter : -5");
+        System.out.println("**tidak pakai margin : -5\n"); 
+    }
+}
